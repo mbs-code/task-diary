02216ecb364233d5b8beffa5d4da07e3e83ecb02
mbs-code <mbscodec@gmail.com>
2022-10-18 18:50:02 +0900

    add: eslint など設定ファイル追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
+import eslint from 'vite-plugin-eslint'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+const isProduct = process.env.NODE_ENV === 'production'
 export default defineNuxtConfig({
   srcDir: 'src/',
   ssr: false,
+
+  app: {
+    head: {
+      script: [
+        !isProduct && { src: 'http://localhost:8098' }, // vue devtools
+      ],
+    },
+  },
 
   vite: {
     clearScreen: false,
@@ -14,5 +25,7 @@ export default defineNuxtConfig({
       minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
       sourcemap: !!process.env.TAURI_DEBUG,
     },
-  }
+
+    plugins: [eslint({ fix: true })],
+  },
 })
