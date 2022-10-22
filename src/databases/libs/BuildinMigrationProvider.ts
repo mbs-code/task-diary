79fa11b23ad5fe647991160e7d49d7d3f13ac620
mbs-code <mbscodec@gmail.com>
2022-10-22 11:38:55 +0900

    add: db plugin を導入

---
+++ src/databases/libs/BuildinMigrationProvider.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/named */
+import {
+  Migration,
+  MigrationProvider
+} from 'kysely'
+import { migrations } from '../Database'
+
+export class BuildinMigrationProvider implements MigrationProvider {
+  // eslint-disable-next-line require-await
+  async getMigrations (): Promise<Record<string, Migration>> {
+    return migrations
+  }
+}
