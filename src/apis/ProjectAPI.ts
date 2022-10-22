acb8367847ba9faa0bec83f0d1919465f2f27db3
mbs-code <mbscodec@gmail.com>
2022-10-22 11:40:13 +0900

    add: project api を作成

---
+++ src/apis/ProjectAPI.ts
@@ -0,0 +1,123 @@
+import { useDayjs } from '~~/src/composables/useDayjs'
+import { Database } from '~~/src/databases/Database'
+import { DBProject, formatProject, FormProject, parseProject, Project } from '~~/src/databases/models/Project'
+
+export type SearchProject = {
+  page?: number
+  limit?: number
+  sorts?: [keyof DBProject, 'asc' | 'desc'][]
+}
+
+export class ProjectAPI {
+  public static async getAll (search?: SearchProject): Promise<Project[]> {
+    const projects = await Database.getDB()
+      .selectFrom('projects')
+      .selectAll()
+      .if(Boolean(search?.page) && Boolean(search.limit), qb => qb.offset((search.page - 1) * search.limit))
+      .if(Boolean(search?.limit), qb => qb.limit(search.limit))
+      .if(Boolean(search?.sorts), qb => search.sorts.reduce(
+        (qb2, sort) => qb2.orderBy(sort[0], sort[1]), qb)
+      )
+      .execute()
+
+    return projects.map(pj => formatProject(pj))
+  }
+
+  public static async get (projectId: number): Promise<Project> {
+    const tag = await Database.getDB()
+      .selectFrom('projects')
+      .selectAll()
+      .where('id', '=', projectId)
+      .executeTakeFirstOrThrow()
+
+    return formatProject(tag)
+  }
+
+  public static async create (form: FormProject): Promise<Project> {
+    const db = Database.getDB()
+    const dayjs = useDayjs()
+
+    const now = dayjs()
+    const parse = parseProject(form)
+
+    // 名前重複確認
+    const { count } = await db
+      .selectFrom('projects')
+      .select([db.fn.count('id').as('count')])
+      .where('name', '=', parse.name)
+      .executeTakeFirst()
+    if (count > 0) { throw new Error('duplicate name') }
+
+    // 作成
+    const { insertId } = await db
+      .insertInto('projects')
+      .values({
+        ...parse,
+        created_at: now.toISOString(),
+        updated_at: now.toISOString(),
+      })
+      .executeTakeFirst()
+
+    return await this.get(Number(insertId))
+  }
+
+  public static async update (projectId: number, form: FormProject): Promise<Project> {
+    const db = Database.getDB()
+    const dayjs = useDayjs()
+
+    const now = dayjs()
+    const parse = parseProject(form)
+
+    // 名前重複確認
+    const { count } = await db
+      .selectFrom('projects')
+      .select([db.fn.count('id').as('count')])
+      .where('name', '=', parse.name)
+      .where('id', '<>', projectId)
+      .executeTakeFirst()
+    if (count > 0) { throw new Error('duplicate name') }
+
+    // 更新
+    const { numUpdatedRows } = await db
+      .updateTable('projects')
+      .set({
+        ...parseProject(form),
+        updated_at: now.toISOString(),
+      })
+      .where('id', '=', projectId)
+      .executeTakeFirst()
+
+    if (Number(numUpdatedRows) === 0) {
+      throw new Error('no result')
+    }
+
+    return await this.get(Number(projectId))
+  }
+
+  public static async remove (projectId: number): Promise<boolean> {
+    const db = Database.getDB()
+
+    // 取ってくる
+    const project = await this.get(projectId)
+
+    // FKで使われているか確認する
+    const { count } = await db
+      .selectFrom('reports')
+      .select([db.fn.count('id').as('count')])
+      .where('project_id', '=', project.id)
+      .executeTakeFirst()
+    if (count > 0) { throw new Error('this has relation') }
+
+    // 削除
+    const { numDeletedRows } = await db
+      .deleteFrom('reports')
+      .where('id', '=', projectId)
+      .executeTakeFirst()
+
+    if (Number(numDeletedRows) === 0) {
+      throw new Error('no result')
+    }
+
+    return true
+  }
+}
--- src/app.vue
+++ src/app.vue
@@ -6,3 +6,39 @@
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup lang="ts">
+import { ProjectAPI } from '~~/src/apis/ProjectAPI'
+import { Database } from '~~/src/databases/Database'
+
+onMounted(async () => {
+  console.log('init')
+  const { migrator } = Database.getInstance()
+  await Database.dbWipe()
+  await migrator.migrateToLatest()
+
+  const p1 = await ProjectAPI.create({
+    name: '案件A',
+    color: '#96245c',
+    icon: 'A',
+  })
+  const p2 = await ProjectAPI.create({
+    name: '案件B',
+    color: '#168d6a',
+    icon: 'B',
+  })
+  const p3 = await ProjectAPI.create({
+    name: 'ZXC案件',
+  })
+  const p4 = await ProjectAPI.create({
+    name: 'Tips',
+  })
+
+  const projects = await ProjectAPI.getAll({
+    sorts: [['created_at', 'desc']],
+    page: 2,
+    limit: 2,
+  })
+  console.log(projects)
+})
+</script>
