f74907cdc27cf33ea28572a84fde36e7581c1f6f
mbs-code <mbscodec@gmail.com>
2022-11-04 22:13:54 +0900

    add: project の編集テーブルを作成

--- src/components/projects/ProjectEditDialog.vue
+++ src/components/projects/ProjectEditDialog.vue
@@ -1,9 +1,8 @@
 <template>
   <Dialog
     v-model:visible="visible"
-    class="w-[600px]"
+    class="p-dialog-maximized"
     modal
-    maximizable
   >
     <template #header>
       <h3>プロジェクト編集</h3>
@@ -11,11 +10,10 @@
 
     <DataTable
       v-model:editingRows="editingRows"
-      :value="projectService.projects.value"
+      :value="projects"
       edit-mode="row"
       data-key="id"
       responsive-layout="scroll"
-      @row-edit-save="onRowEditSave"
     >
       <Column field="id" header="ID">
         <!-- <template #editor="{ data, field }">
@@ -41,61 +39,39 @@
         </template>
       </Column>
 
-      <Column :row-editor="true" style="width:10%; min-width:8rem" body-style="text-align:center" />
-      <!--
-        v-model:editingRows="editingRows"
-      @row-edit-save="onRowEditSave" -->
-      <!-- <Column field="code" header="Code" style="width:20%">
-        <template #editor="{ data, field }">
-          <InputText v-model="data[field]" autofocus />
+      <Column>
+        <template #header>
+          <Button icon="pi pi-plus" class="p-button-rounded p-button-text p-button-plain" @click="onRowAdd" />
+        </template>
+
+        <template #body="{ data }">
+          <template v-if="!isRowEdit(data)">
+            <Button icon="pi pi-pencil" class="p-button-rounded p-button-text p-button-plain" @click="onRowEdit(data)" />
+          </template>
+
+          <template v-else>
+            <Button icon="pi pi-check" class="p-button-rounded p-button-text p-button-plain" @click="onRowEditSave(data)" />
+            <Button icon="pi pi-times" class="p-button-rounded p-button-text p-button-plain" @click="onRowEditCancel(data)" />
+          </template>
         </template>
       </Column>
-      <Column field="name" header="Name" style="width:20%">
-        <template #editor="{ data, field }">
-          <InputText v-model="data[field]" />
-        </template>
-      </Column>
-      <Column field="inventoryStatus" header="Status" style="width:20%">
-        <template #editor="{ data, field }">
-          <Dropdown v-model="data[field]" :options="statuses" option-label="label" option-value="value" placeholder="Select a Status">
-            <template #option="slotProps">
-              <span :class="'product-badge status-' + slotProps.option.value.toLowerCase()">{{ slotProps.option.label }}</span>
-            </template>
-          </Dropdown>
-        </template>
-        <template #body="slotProps">
-          {{ getStatusLabel(slotProps.data.inventoryStatus) }}
-        </template>
-      </Column>
-      <Column field="price" header="Price" style="width:20%">
-        <template #editor="{ data, field }">
-          <InputText v-model="data[field]" />
-        </template>
-      </Column>
-      <Column :row-editor="true" style="width:10%; min-width:8rem" body-style="text-align:center" /> -->
     </DataTable>
   </Dialog>
 </template>
 
 <script setup lang="ts">
-import { DataTableRowEditSaveEvent } from 'primevue/datatable'
 import { inject } from 'vue'
 import { ProjectAPI } from '~~/src/apis/ProjectAPI'
-import { ReportAPI } from '~~/src/apis/ReportAPI'
 import { FormProject, Project } from '~~/src/databases/models/Project'
-import { FormReport, Report } from '~~/src/databases/models/Report'
-import { Status } from '~~/src/databases/models/Status'
 
 const props = defineProps<{
   visible: boolean,
-  // report?: Report,
-  // projects: Project[],
 }>()
 
 // eslint-disable-next-line func-call-spacing
 const emit = defineEmits<{
   (e: 'update:visible', visible: boolean),
-  // (e: 'update:report', report: Report, oldReport?: Report),
+  (e: 'update:project', project: Project, old?: Project),
 }>()
 
 const visible = computed({
@@ -107,25 +83,73 @@ const projectService = inject(ProjectServiceKey)
 
 /// ////////////////////////////////////////
 
-const editingRows = ref([])
+const projects = ref<Project[]>([])
+const editingRows = ref<Project[]>([])
 
-const onRowEditSave = async (event: DataTableRowEditSaveEvent) => {
-  const data = event.data as Project
-  const newData = event.newData as Project
+const init = () => {
+  editingRows.value = []
+  projects.value = [...projectService.projects.value]
+}
 
+watch(() => props.visible, (val) => {
+  val && init()
+})
+
+/// ////////////////////////////////////////
+
+const isRowEdit = (project: Project) => {
+  return editingRows.value.some(row => row.id === project.id)
+}
+
+const onRowAdd = () => {
+  // 新規rowがなければ追加
+  const hasNewRow = projects.value.some(p => p.id === 0)
+  if (!hasNewRow) {
+    const newProject = {
+      id: 0,
+      name: '',
+      createdAt: undefined,
+      updatedAt: undefined,
+    }
+    projects.value.push(newProject)
+    editingRows.value = [...editingRows.value, newProject]
+  }
+}
+
+const onRowEdit = (project: Project) => {
+  // 編集中に追加されていなければ追加
+  const hasEditRow = editingRows.value.some(row => row.id === project.id)
+  if (!hasEditRow) {
+    editingRows.value = [...editingRows.value, project]
+  }
+}
+
+const onRowEditCancel = (project: Project) => {
+  // 同idを取り除く
+  editingRows.value = editingRows.value.filter(row => row.id !== project.id)
+  if (project.id === 0) {
+    projects.value = projects.value.filter(p => p.id !== project.id)
+  }
+}
+
+const onRowEditSave = async (project: Project) => {
   const params: FormProject = {
-    name: newData.name,
-    color: newData.color,
-    icon: newData.icon,
+    name: project.name,
+    color: project.color,
+    icon: project.icon,
   }
 
   // upsert 処理
-  const projectId = data?.id
-  const _updProject = projectId
+  const projectId = project?.id
+  const updProject = projectId
     ? await ProjectAPI.update(projectId, params)
     : await ProjectAPI.create(params)
 
   // 画面更新
   projectService.fetch()
+  init()
+
+  const oldProject = editingRows.value.find(row => row.id === project.id)
+  emit('update:project', updProject, oldProject)
 }
 </script>
